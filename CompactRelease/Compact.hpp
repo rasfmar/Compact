17c7b1be315724e5ba7292a969f766705d544bbc
Marcus Franco <rasfmar@rasfmar-2.local>
2020-05-15 02:40:13 -0400

    v0.13_1 - Literally just fixed the version number

--- CompactRelease/Compact.hpp
+++ CompactRelease/Compact.hpp
@@ -3,7 +3,7 @@
 #ifndef COMPACT
 #define COMPACT
 
-#define COMPACT_VERSION() { return "Compact v0.130"}
+#define COMPACT_VERSION() { return "Compact v0.13_1"}
 
 #include "Compact_State.hpp"
 #include "Compact_Interpret.hpp"
